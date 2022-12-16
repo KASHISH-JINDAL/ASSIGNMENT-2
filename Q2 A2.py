eb96f42302d7532e651cc73887519805291c9c61
KASHISH-JINDAL <120741517+KASHISH-JINDAL@users.noreply.github.com>
2022-12-16 22:39:32 +0530

    Add files via upload

---
+++ Q2 A2.py
@@ -0,0 +1,8 @@
+name = "Kashish Jindal"
+sid = 22104029
+department_name = "Electrical Engineering"
+cgpa = 10.0
+print("Hey," , name , "Here!")
+print("My SID is", sid )
+print("I am from" , department_name , "department and my CGPA is" , cgpa)
+

69847d65dd433b14b88737b2d9b5fd493f697b3a
KASHISH-JINDAL <120741517+KASHISH-JINDAL@users.noreply.github.com>
2022-12-22 12:38:43 +0530

    Update Q4 A2.py

--- Q4 A2.py
+++ Q4 A2.py
@@ -1,6 +1,6 @@
-num1 = int(input("Enter the First Number"))
-num2 = int(input("Enter the Second Number"))
-num3 = int(input("Enter the Third Number"))
+num1 = float(input("Enter the First Number"))
+num2 = float(input("Enter the Second Number"))
+num3 = float(input("Enter the Third Number"))
 if (num1 >= num2) and (num1 >= num3):
    largest = num1
 elif (num2 >= num1) and (num2 >= num3):
@@ -9,3 +9,6 @@ else:
    largest = num3
 
 print("The largest number is", largest)
+
+print("Or we can simply use a max() function to compare these numbers.")
+print("The Largest number is", max(num2,num1,num3))
